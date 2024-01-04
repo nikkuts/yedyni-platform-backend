919289861410d75fd65c7bd31012b6142482a7e5
nikkuts <nickkuts888@gmail.com>
2024-01-04 17:44:50 +0200

    Update payments.js

--- controllers/payments.js
+++ controllers/payments.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 const PUBLIC_KEY = process.env.PUBLIC_KEY_TEST;
 const PRIVATE_KEY = process.env.PRIVATE_KEY_TEST;
 const {BASE_CLIENT_URL, BASE_SERVER_URL} = process.env;
+const MAIN_ID = process.env.MAIN_ID;
 
 const createPayment = async (req, res) => {
     const {_id} = req.user;
@@ -43,9 +44,8 @@ const createPayment = async (req, res) => {
 };
 
 const distributesBonuses = async (id, amount) => {
-  const MAIN_ID = process.env.MAIN_ID; 
+  let inviterId = id; 
   let bonus = amount * 0.45;
-  let inviterId = id;
   let userId;
   let bonusAccount;
   let level;
@@ -67,10 +67,6 @@ const distributesBonuses = async (id, amount) => {
           bonusAccount = user.bonusAccount;
           level = levelSupport(user);
       } while (level < i);
-
-      // if (userId === MAIN_ID) {
-      //     return { success: true, message: 'Main user reached' };
-      // }
 
       fee = i === 1 
           ? amount * 0.1
@@ -119,7 +115,9 @@ const processesPayment = async (req, res) => {
         { new: true }
       );
 
-      await distributesBonuses (user.inviter, amount);
+      if (customer !== MAIN_ID) {
+        await distributesBonuses (user.inviter, amount);
+      }
     }
 
     res.status(200).json({
