e605c0971e6114364230b22618df24f969f2ecef
nikkuts <nickkuts888@gmail.com>
2024-02-16 19:10:04 +0200

    Update paymentsClient.js

--- controllers/paymentsClient.js
+++ controllers/paymentsClient.js
@@ -18,15 +18,80 @@ const getDonats = async (req, res) => {
 
 const getSubscriptions = async (req, res) => {
     const {_id} = req.user;
-    const result = await User.findById(_id, "subscriptions -_id")
-    .populate('subscriptions', 
-        '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe');
-    
-    if(!result) {
-        throw HttpError (404, 'Not found')
+    const { start = null, end = null } = req.query;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+    let result;
+
+    if (start && end) {
+        result = await Payment.find(
+            {
+                'data.customer': _id,
+                'data.action': 'subscribe',
+                'data.status': 'subscribed',
+                $and: [
+                    {'data.end_date': {$gte: Number(start)}}, 
+                    {'data.end_date': {$lte: Number(end)}}
+                ]
+            },
+            '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe',
+            { skip, limit }
+        )
+    } else {
+        result = await Payment.find(
+            {
+                'data.customer': _id,
+                'data.action': 'subscribe',
+                'data.status': 'subscribed',
+            },
+            '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe',
+            { skip, limit }
+        )
     }
+
+    if (result.length === 0) {
+        throw HttpError(404, "Не знайдено");
+    }
+
+
+    // let result;
+
+    // if (start && end) {
+    //     result = await User.findById(
+    //         { _id, subscriptions: {  } },
+    //         "subscriptions -_id",
+    //         { skip, limit }
+    //     )
+    //     .populate('subscriptions', 
+    //         '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe');
+    // } else {
+    //     result = await User.findById(
+    //         _id,
+    //         "subscriptions -_id",
+    //         { skip, limit }
+    //     )
+    //     .populate('subscriptions', 
+    //         '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe');
+    // }
+
+    // if (result.length === 0) {
+    //     throw HttpError(404, "Не знайдено");
+    // }
+
     res.json(result);
 };
+
+// const getSubscriptions = async (req, res) => {
+//     const {_id} = req.user;
+//     const result = await User.findById(_id, "subscriptions -_id")
+//     .populate('subscriptions', 
+//         '_id data.order_id data.amount data.end_date data.description data.info objSub.lastPaymentDate objSub.isUnsubscribe');
+    
+//     if(!result) {
+//         throw HttpError (404, 'Not found')
+//     }
+//     res.json(result);
+// };
 
 const getByIdSubscription = async (req, res) => {
     // const {subscriptionId} = req.params;
