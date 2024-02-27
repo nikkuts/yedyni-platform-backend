79cec41ae8e565dac7780ebb9c26788ce2a146c8
nikkuts <nickkuts888@gmail.com>
2024-02-27 14:04:22 +0200

    Update exercises.js

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -9,7 +9,7 @@ const getExercise = async (req, res) => {
   const {_id: owner} = req.user;
   const { courseId, lessonId } = req.query;
   // const {courseId, lessonId} = req.body;
-console.log(courseId, lessonId);
+console.log(req.query);
   const result = await Exercises.findOne(
     { owner, courseId, lessonId }, 
     "-createdAt -updatedAt"
