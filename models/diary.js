858221823e0755734cd0b0baac6bfd8ba9e2ed28
nikkuts <nickkuts888@gmail.com>
2024-04-25 20:30:36 +0300

    Update diary.js

--- models/diary.js
+++ models/diary.js
@@ -43,8 +43,8 @@ const addDiarySchema = Joi.object({
   lessonId: Joi.string().required(),
   date: Joi.string().required(),
   test: Joi.number().integer().min(0).max(10),
-  entry: Joi.string().max(500).required(),
-  plan: Joi.string().max(500).required(),
+  entry: Joi.string().max(500).allow('').required(),
+  plan: Joi.string().max(500).allow('').required(),
 });
 
 const schemas = {
