85aa53dfa6a9cc5eb5f803d5675969dc8a3ebe10
nikkuts <nickkuts888@gmail.com>
2024-05-25 19:31:27 +0300

    add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://teachinukrainian:T8zwvjNIO2BqYolb@cluster0.idre1eh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+module.exports = {
+    DB_HOST,
+}
--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const { DB_HOST, PORT = 3000 } = process.env;
+// const {DB_HOST} = require('./config');
 
 mongoose.set('strictQuery', true);
 mongoose.Promise = global.Promise;
 
-const mongoUri = process.env.NODE_ENV === 'test' ? 'mongodb://localhost/testdb' : DB_HOST;
+// const mongoUri = process.env.NODE_ENV === 'test' ? 'mongodb://localhost/testdb' : DB_HOST;
 
-mongoose.connect(mongoUri)
+mongoose.connect(DB_HOST)
   .then(() => {
     console.log('Database connection successful');
-    app.listen(PORT);
+    app.listen(3000);
   })
   .catch(error => {
     console.log(error.message);
