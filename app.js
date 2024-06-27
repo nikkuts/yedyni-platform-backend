beca38a051cb689ea920fabb9f9ef288799d5987
nikkuts <nickkuts888@gmail.com>
2024-06-27 14:09:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ const app = express()
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
-app.use(cors({origin: ['https://yedyni.org', 'https://www.liqpay.ua', 'https://yedyni.uspacy.ua']}))
+app.use(cors())
+// app.use(cors({origin: ['https://yedyni.org', 'https://www.liqpay.ua', 'https://yedyni.uspacy.ua']}))
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
