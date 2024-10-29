3d9b77ebc4d6390b4eee203c8e05a8bd0ccd9b7c
Mykola Kuts <nickkuts888@gmail.com>
2024-10-29 20:56:20 +0200

    Update canals.js

--- controllers/canals.js
+++ controllers/canals.js
@@ -6,39 +6,28 @@ const { TELEGRAM_BOT_TOKEN } = process.env;
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
 // Функція надсилання привітання новому учаснику в групу
-    const sendWelcomeMessage = async (message) => { 
-        if (message.new_chat_member) {
-            const chatId = message.chat.id;
-            const newMember = message.new_chat_member;
+const sendWelcomeMessage = async (message) => { 
+    if (message.new_chat_member) {
+        const chatId = message.chat.id;
+        const newMember = message.new_chat_member;
+        const welcomeMessage = `Ласкаво просимо, ${newMember.first_name}! 🎉 Напишіть асистенту [тут](https://t.me/YedinyBot), щоб отримати ваш подарунок.`;
+        
+        await bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
+    }
+};
 
-            const welcomeMessage = `Ласкаво просимо, ${newMember.first_name}! 🎉 Напишіть боту в особисті повідомлення [тут](https://t.me/YedinyBot), щоб отримати ваш подарунок.`;
-            
-            await bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
-        }
-    };
+// Функція для обробки особистого повідомлення та надсилання подарунка
+const handlePrivateMessage = async (message) => {
+    if (message.chat.type === 'private') {
+        const chatId = message.chat.id;
+        const giftFilePath = 'https://res.cloudinary.com/dwnbra6yc/image/upload/v1727889954/lm2hlqoe4ojvev4iat1b.png';
 
-    // Функція для обробки особистого повідомлення та надсилання подарунка
-    const handlePrivateMessage = async (message) => {
-        if (message.chat.type === 'private') {
-            const chatId = message.chat.id;
-            const giftFilePath = 'https://res.cloudinary.com/dwnbra6yc/image/upload/v1727889954/lm2hlqoe4ojvev4iat1b.png';
-
-            await bot.sendDocument(chatId, giftFilePath, { caption: "Вітаємо на курсі від руху 'Єдині'! 🎉 Ось ваш подарунок!" });
-        }
-    };
+        await bot.sendDocument(chatId, giftFilePath, { caption: "Вітаємо на курсі від руху 'Єдині'! 🎉 Ось ваш подарунок!" });
+    }
+};
 
 const sendGift = async (req, res) => { 
     const { message } = req.body;
-    console.log('message', message);
-
-   /*  if (message && message.new_chat_member) {
-        const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
-
-        const chatId = message.chat.id;
-        const newMember = message.new_chat_member;
-        const welcomeMessage = `Ласкаво просимо, ${newMember.first_name}! 🎉 Натисніть [тут](https://t.me/YedinyBot), щоб отримати ваш подарунок.`;
-        await bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
-    } */
     
     // Виконуємо sendWelcomeMessage для нових учасників
     if (message && message.new_chat_member) {
@@ -53,23 +42,6 @@ const sendGift = async (req, res) => {
     res.sendStatus(200);
 };
 
-/* const sendGift = async (req, res) => { 
-    const { message } = req.body;
-    console.log('message', message);
-
-    if (message && message.chat) {
-        const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
-        
-        const chatId = message.chat.id;
-        const giftFilePath = 'https://res.cloudinary.com/dwnbra6yc/image/upload/v1727889954/lm2hlqoe4ojvev4iat1b.png';
-
-        bot.sendDocument(chatId, giftFilePath, { caption: "Вітаємо на курсі від руху 'Єдині'! 🎉 Ось ваш подарунок!" });
-    }
-
-    res.sendStatus(200);
-}; */
-
 module.exports = {
-    // sendWelcomeMessage: ctrlWrapper(sendWelcomeMessage),
     sendGift: ctrlWrapper(sendGift),
 };
