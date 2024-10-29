f9eac88a25ea0ebfd0a2f96ea302df0d174a2b20
Mykola Kuts <nickkuts888@gmail.com>
2024-10-29 18:23:25 +0200

    Update canals.js

--- controllers/canals.js
+++ controllers/canals.js
@@ -11,10 +11,15 @@ const sendGift = async (req, res) => {
         const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
         console.log('message', message);
         
-        const chatId = message.new_chat_member.id;
+        /* const chatId = message.new_chat_member.id;
         const giftFilePath = 'https://res.cloudinary.com/dwnbra6yc/image/upload/v1727889954/lm2hlqoe4ojvev4iat1b.png';
 
-        bot.sendDocument(chatId, giftFilePath, { caption: "Вітаємо на курсі від руху 'Єдині'! 🎉 Ось ваш подарунок!" });
+        bot.sendDocument(chatId, giftFilePath, { caption: "Вітаємо на курсі від руху 'Єдині'! 🎉 Ось ваш подарунок!" }); */
+
+        const chatId = message.chat.id;
+        const newMember = message.new_chat_member;
+        const welcomeMessage = `Ласкаво просимо, ${newMember.first_name}! 🎉 Натисніть [тут](https://t.me/YedyniBot), щоб отримати ваш подарунок.`;
+        bot.sendMessage(chatId, welcomeMessage, { parse_mode: 'Markdown' });
     }
 
     res.sendStatus(200);
