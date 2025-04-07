541f5d43dcc2861e4bbecdf8f3943f253be84679
Mykola Kuts <nickkuts888@gmail.com>
2025-04-07 17:30:27 +0300

    Update regexp.js

--- utils/regexp.js
+++ utils/regexp.js
@@ -4,7 +4,7 @@ const locationRegexp = /^[A-Z][a-z]+$/;
 const nameRegexp = /^[a-zA-Zа-яА-ЯїЇіІєЄґҐ\s]{2,30}$/u;
 const phoneRegexp = /^(\+38)?0\d{9}$/;
 const workScheduleRegexp = /^([01]\d|2[0-3]):[0-5]\d$/;
-const passwordRegex = /^[a-zA-Z0-9]{8,24}$/;
+const passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,24}$/;
 // const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,24}$/;
 
 module.exports = {
